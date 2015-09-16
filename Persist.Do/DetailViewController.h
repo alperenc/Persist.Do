26870f30c1dd1ad49f3da7df58e6337a6330771d
Alp Eren Can <alperenc@gmail.com>
2015-09-16 11:46:17 -0700

    Initial Commit

---
+++ Persist.Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Persist.Do
+//
+//  Created by Alp Eren Can on 16/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Persist.Do/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  Persist.Do
+//
+//  Created by Alp Eren Can on 16/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
